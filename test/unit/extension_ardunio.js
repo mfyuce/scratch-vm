7c443e8aa03dee0977fc0c2ef100275e4b3d0fa1
Mehmet.Yuce <mehmet.yuce@logosgt.com.tr>
2019-10-03 19:31:37 +0300

    first ardunio checkin

---
+++ test/unit/extension_ardunio.js
@@ -0,0 +1,12 @@
+const test = require('tap').test;
+// const Ardunio = require('../../src/extensions/scratch3_ardunio/index.js');
+
+test('displayText', t => {
+    t.end();
+});
+
+test('displayMatrix', t => {
+    t.end();
+});
+
+// etc...
